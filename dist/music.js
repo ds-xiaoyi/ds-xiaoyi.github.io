e60097295f390647b64796b3864e0ee14ba9a83b
ds_xiaoyi <13010331+ds-xiaoyi@user.noreply.gitee.com>
2023-06-11 07:29:37 +0800

    Site updated: 2023-06-11 07:29:37

--- dist/music.js
+++
@@ -1,9 +0,0 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    audio: [{
-        name: 'Minecraft',
-        artist: 'C418',
-        url: 'Minecraft.mp3',
-        cover: 'Minecraft.jpg'
-    }]
-});
